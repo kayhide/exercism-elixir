3ca9d465cc88d1bfed3feb93cda816de8b537001
Hideaki Kawai <kayhide@gmail.com>
2020-05-22 23:21:16 +0900

    Solve protein-translation

---
+++ protein-translation/lib/protein_translation.ex
@@ -0,0 +1,65 @@
+defmodule ProteinTranslation do
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    Regex.scan(~r/.../, rna)
+    |> Enum.map(fn [x] -> of_codon(x) end)
+    |> Enum.reduce({:cont, []}, &reduce/2)
+    |> finalize()
+  end
+
+  defp reduce({:ok, "STOP"}, {:cont, cs}), do: {:ok, cs}
+  defp reduce({:ok, c}, {:cont, cs}), do: {:cont, cs ++ [c]}
+  defp reduce(_, {:ok, cs}), do: {:ok, cs}
+  defp reduce(_, _), do: {:error, "invalid RNA"}
+
+  defp finalize({:cont, cs}), do: {:ok, cs}
+  defp finalize(x), do: x
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case codon do
+      "UGU" -> {:ok, "Cysteine"}
+      "UGC" -> {:ok, "Cysteine"}
+      "UUA" -> {:ok, "Leucine"}
+      "UUG" -> {:ok, "Leucine"}
+      "AUG" -> {:ok, "Methionine"}
+      "UUU" -> {:ok, "Phenylalanine"}
+      "UUC" -> {:ok, "Phenylalanine"}
+      "UCU" -> {:ok, "Serine"}
+      "UCC" -> {:ok, "Serine"}
+      "UCA" -> {:ok, "Serine"}
+      "UCG" -> {:ok, "Serine"}
+      "UGG" -> {:ok, "Tryptophan"}
+      "UAU" -> {:ok, "Tyrosine"}
+      "UAC" -> {:ok, "Tyrosine"}
+      "UAA" -> {:ok, "STOP"}
+      "UAG" -> {:ok, "STOP"}
+      "UGA" -> {:ok, "STOP"}
+      _ -> {:error, "invalid codon"}
+    end
+  end
+end
