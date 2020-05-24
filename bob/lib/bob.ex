77697842d4bd27d37d59873e4251d0697e5604f3
Hideaki Kawai <kayhide@gmail.com>
2020-05-24 15:19:02 +0900

    Refactor bob

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -2,10 +2,10 @@ defmodule Bob do
   def hey(input) do
     state =
       String.graphemes(input)
-      |> Enum.reduce(%{upper: false, lower: false, digit: false, asking: false}, &f/2)
+      |> Enum.reduce(%{upper: false, lower: false, other: false, asking: false}, &f/2)
 
     case state do
-      %{upper: false, lower: false, digit: false, asking: false} -> "Fine. Be that way!"
+      %{upper: false, lower: false, other: false, asking: false} -> "Fine. Be that way!"
       %{upper: true, lower: false, asking: true} -> "Calm down, I know what I'm doing!"
       %{upper: true, lower: false} -> "Whoa, chill out!"
       %{asking: true} -> "Sure."
@@ -15,11 +15,15 @@ defmodule Bob do
 
   defp f(c, state) do
     cond do
-      String.match?(c, ~r/[[:lower:]]/) -> %{state | lower: true, asking: false}
-      String.match?(c, ~r/[[:upper:]]/) -> %{state | upper: true, asking: false}
-      String.match?(c, ~r/[[:digit:]]/) -> %{state | digit: true, asking: false}
-      String.match?(c, ~r/\?/) -> %{state | asking: true}
-      true -> state
+      c == "?" -> %{state | asking: true}
+      is_lower?(c) -> %{state | lower: true, asking: false}
+      is_upper?(c) -> %{state | upper: true, asking: false}
+      is_blank?(c) -> state
+      true -> %{state | other: true, asking: false}
     end
   end
+
+  defp is_lower?(c), do: c != String.upcase(c)
+  defp is_upper?(c), do: c != String.downcase(c)
+  defp is_blank?(c), do: "" == String.trim(c)
 end
