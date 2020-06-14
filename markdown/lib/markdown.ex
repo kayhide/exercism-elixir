0f99acb9343bf9607c079f41f4b9334944a3962b
Hideaki Kawai <kayhide@gmail.com>
2020-06-14 17:56:58 +0900

    Refactor markdown modularizing and using typespecs

--- markdown/lib/markdown.ex
+++ markdown/lib/markdown.ex
@@ -1,29 +1,222 @@
-defmodule Markdown do
+defmodule Functor do
+  # f a -> (a -> b) -> f b
+  @callback fmap(term, (term -> term)) :: term
+end
+
+defmodule Applicative do
+  import Functor
+  # a -> f a
+  @callback pure(term) :: term
+  # f a -> f (a -> b) -> f b
+  @callback app(term, term) :: term
+end
+
+defmodule Alternative do
+  import Functor
+  # f a
+  @callback empty() :: term
+  # f a -> f a -> f a
+  @callback alt(term, term) :: term
+
   @doc """
-    Parses a given string with Markdown syntax and returns the associated HTML for that string.
+  Implemented in terms of Monad
+  """
+  # m a -> m [a]
+  @spec some(module, term) :: term
+  def some(impl, p) do
+    p
+    |> impl.bind(fn x ->
+      impl.alt(impl.fmap(some(impl, p), &[x | &1]), impl.pure([x]))
+    end)
+  end
 
-    ## Examples
+  # [m a] -> m a
+  @spec asum(module, [term]) :: term
+  def asum(impl, ps), do: Enum.reduce(ps, impl.empty(), &impl.alt(&1, &2))
+end
 
-    iex> Markdown.parse("This is a paragraph")
-    "<p>This is a paragraph</p>"
+defmodule Monad do
+  import Applicative
+  @callback bind(term, term) :: term
+end
 
-    iex> Markdown.parse("#Header!\n* __Bold Item__\n* _Italic Item_")
-    "<h1>Header!</h1><ul><li><em>Bold Item</em></li><li><i>Italic Item</i></li></ul>"
+defmodule Parser do
+  @behaviour Functor
+  @behaviour Applicative
+  @behaviour Alternative
+  @behaviour Monad
+
+  @type t(a) :: %__MODULE__{run: (String.t() -> {a, String.t()} | :fail)}
+
+  defstruct run: &Parser.fail/1
+
+  @spec fail(String.t()) :: :fail
+  def fail(_), do: :fail
+
+  @spec run_parser(t(String.t()), String.t()) :: String.t() | :fail
+  def run_parser(%Parser{run: run}, str) do
+    case run.(str) do
+      :fail -> :fail
+      {res, _} -> res
+    end
+  end
+
+  @impl Functor
+  @spec fmap(t(a), (a -> b)) :: t(b) when a: term, b: term
+  def fmap(%Parser{run: run}, f) do
+    %Parser{
+      run: fn str ->
+        case run.(str) do
+          :fail -> :fail
+          {y, str_} -> {f.(y), str_}
+        end
+      end
+    }
+  end
+
+  @impl Applicative
+  def pure(x) do
+    %Parser{
+      run: fn str -> {x, str} end
+    }
+  end
+
+  @impl Applicative
+  def app(%Parser{run: x}, %Parser{run: f}) do
+    %Parser{
+      run: fn str -> {f.(x), str} end
+    }
+  end
+
+  @impl Alternative
+  def empty(), do: %Parser{}
+
+  @impl Alternative
+  def alt(%Parser{run: p1}, %Parser{run: p2}) do
+    %Parser{
+      run: fn str ->
+        case p1.(str) do
+          :fail -> p2.(str)
+          res -> res
+        end
+      end
+    }
+  end
+
+  @impl Monad
+  def bind(%Parser{run: run}, act) do
+    %Parser{
+      run: fn str ->
+        case run.(str) do
+          :fail ->
+            :fail
+
+          {y, str_} ->
+            case act.(y) do
+              %Parser{run: run2} -> run2.(str_)
+            end
+        end
+      end
+    }
+  end
+end
+
+defmodule PrimitiveParsers do
+  @moduledoc """
+  Primitive and helper parsers
+  """
+
+  import Alternative
+  import Monad
+  import Parser
+
+  @spec satisfy((String.t() -> boolean)) :: Parser.t(String.t())
+  def satisfy(pred) do
+    %Parser{
+      run: fn str ->
+        {x, xs} = String.split_at(str, 1)
+        if pred.(x), do: {x, xs}, else: :fail
+      end
+    }
+  end
+
+  @spec chunk(String.t()) :: Parser.t(String.t())
+  def chunk(x) do
+    %Parser{
+      run: fn str ->
+        case String.split_at(str, String.length(x)) do
+          {^x, xs} -> {x, xs}
+          _ -> :fail
+        end
+      end
+    }
+  end
+
+  @spec eof() :: Parser.t(nil)
+  def eof() do
+    %Parser{
+      run: fn
+        "" -> {nil, ""}
+        _ -> :fail
+      end
+    }
+  end
+
+  @spec symbol(Parser.t(String.t())) :: Parser.t(String.t())
+  def symbol(p) do
+    p
+    |> bind(fn x ->
+      some(Parser, chunk(" "))
+      |> fmap(fn _ -> x end)
+    end)
+  end
+
+  @spec eol() :: Parser.t(nil)
+  def eol() do
+    alt(chunk("\n"), eof())
+  end
+
+  @spec between(Parser.t(String.t()), Parser.t(String.t())) :: Parser.t(String.t())
+  def between(p1, p2) do
+    p1
+    |> bind(fn _ ->
+      p2
+      |> bind(fn x ->
+        p1
+        |> fmap(fn _ -> x end)
+      end)
+    end)
+  end
+end
+
+defmodule Markdown do
+  import Alternative
+  import Parser
+  import PrimitiveParsers
+
+  @doc """
+  Parses a given string with Markdown syntax and returns the associated HTML for that string.
+
+  ## Examples
+
+  iex> Markdown.parse("This is a paragraph")
+  "<p>This is a paragraph</p>"
+
+  iex> Markdown.parse("#Header!\n* __Bold Item__\n* _Italic Item_")
+  "<h1>Header!</h1><ul><li><em>Bold Item</em></li><li><i>Italic Item</i></li></ul>"
   """
   @spec parse(String.t()) :: String.t()
   def parse(m) do
     run_parser(document(), m)
   end
 
-  # parsers
-
   defp document() do
-    some(asum([header(), list(), paragraph()]))
+    some(Parser, asum(Parser, [header(), list(), paragraph()]))
     |> fmap(&Enum.join/1)
   end
 
   defp header() do
-    symbol(some(chunk("#")))
+    symbol(some(Parser, chunk("#")))
     |> bind(fn n ->
       line()
       |> fmap(tag("h#{length(n)}"))
@@ -31,7 +224,7 @@ defmodule Markdown do
   end
 
   defp list() do
-    some(list_item())
+    some(Parser, list_item())
     |> fmap(&tag("ul").(Enum.join(&1)))
   end
 
@@ -59,138 +252,22 @@ defmodule Markdown do
   end
 
   defp line() do
-    some(asum([strong(), italic(), text()]))
+    some(Parser, asum(Parser, [strong(), italic(), text()]))
     |> bind(fn x ->
       eol()
       |> fmap(fn _ -> x end)
     end)
   end
 
-  # helper parsers
-
   defp text() do
-    some(satisfy(&String.match?(&1, ~r/[0-9a-zA-Z !]/)))
+    some(Parser, satisfy(&String.match?(&1, ~r/[0-9a-zA-Z !]/)))
     |> fmap(&Enum.join/1)
   end
 
-  defp symbol(p) do
-    p
-    |> bind(fn x ->
-      some(chunk(" "))
-      |> fmap(fn _ -> x end)
-    end)
-  end
-
-  defp eol() do
-    alt(chunk("\n"), eof())
-  end
-
-  defp between(p1, p2) do
-    p1
-    |> bind(fn _ ->
-      p2
-      |> bind(fn x ->
-        p1
-        |> fmap(fn _ -> x end)
-      end)
-    end)
-  end
-
-  # helper functions
-
+  @spec tag(String.t()) :: (String.t() -> String.t())
   defp tag(key) do
     fn text ->
       "<#{key}>#{text}</#{key}>"
-    end
-  end
-
-  # parser monad
-  # m a :: string -> {a, string} | :fail
-
-  # run_parser :: m a -> string | :fail
-  defp run_parser(p, str) do
-    case p.(str) do
-      :fail -> :fail
-      {res, _} -> res
-    end
-  end
-
-  # pure :: a -> m a
-  defp pure(x) do
-    fn str -> {x, str} end
-  end
-
-  # fmap :: m a -> (a -> b) -> m b
-  defp fmap(x, f) do
-    fn str ->
-      case x.(str) do
-        :fail -> :fail
-        {y, str_} -> {f.(y), str_}
-      end
-    end
-  end
-
-  # bind :: m a -> (a -> m b) -> m b
-  defp bind(x, act) do
-    fn str ->
-      case x.(str) do
-        :fail -> :fail
-        {y, str_} -> act.(y).(str_)
-      end
-    end
-  end
-
-  defp fail() do
-    fn _ -> :fail end
-  end
-
-  # alternative
-
-  # alt :: m a -> m a -> m a
-  defp alt(p1, p2) do
-    fn str ->
-      case p1.(str) do
-        :fail -> p2.(str)
-        res -> res
-      end
-    end
-  end
-
-  # some :: m a -> m [a]
-  defp some(p) do
-    p
-    |> bind(fn x ->
-      alt(fmap(some(p), &[x | &1]), pure([x]))
-    end)
-  end
-
-  # asum :: [m a] -> m a
-  defp asum(ps), do: Enum.reduce(ps, fail(), &alt(&1, &2))
-
-  # primitives
-
-  # satisfy :: (a -> bool) -> m a
-  defp satisfy(pred) do
-    fn str ->
-      {x, xs} = String.split_at(str, 1)
-      if pred.(x), do: {x, xs}, else: :fail
-    end
-  end
-
-  # chunk :: a -> m a
-  defp chunk(x) do
-    fn str ->
-      case String.split_at(str, String.length(x)) do
-        {^x, xs} -> {x, xs}
-        _ -> :fail
-      end
-    end
-  end
-
-  defp eof() do
-    fn
-      "" -> {nil, ""}
-      _ -> :fail
     end
   end
 end
